c0a25a238cc530ae62f867f6fd24f070981dc920
harooks <haruko.flipsy.krb@gmail.com>
2022-04-22 13:02:31 -0500

    organized file, added climb and penalty

---
+++ Final Project/agent.py
@@ -0,0 +1,48 @@
+import enum
+import random
+
+class Agent:
+
+  def __init__(self, q_a, q_b, action_arr, ex, reward, alpha, gamma):
+    self.q_a = q_a
+    self.q_b = q_b
+    self.action_arr = action_arr
+    self.ex = ex
+
+  def choose_action(self):
+
+    # Greedy
+    if random.random() > self.ex:
+        a_or_b = random.randint(0,1)
+
+        if a_or_b == 0:
+          val = max(self.q_a)
+          action = self.q_a.index(val)
+        else:
+          val = max(self.q_b)
+          action = self.q_b.index(val)
+
+    # Random
+    else:
+        # index of action choice chosen randomly
+        action = random.randrange(0, len(self.action_arr))
+        a_or_b = random.randint(0, 1)
+
+        if a_or_b == 0: #A
+          val = self.q_a[action]  # the q value of the action chosen
+        else: #B
+          val = self.q_b[action]
+
+    # Q[cur_row][cur_col][action] = val + \
+       # (alpha * (reward + (gamma * max(Q[next_row][next_col])) - val))
+
+    return [a_or_b, action, val]
+
+    # Get reward from enviroment
+
+  def update_Qtable(a_or_b, action, val, reward, alpha, gamma, q_a, q_b):
+
+    if a_or_b == 0: #A
+      q_a[action] = val + (alpha * (reward + (gamma * max(q_b[action]) - val)))
+    else:
+      q_b[action] = val + (alpha * (reward + (gamma * max(q_a[action]) - val)))
---
+++ Final Project/climb_and_penalty.py
@@ -0,0 +1,9 @@
+from agent import Agent
+
+# make agent
+
+# set reward based on action, reward,
+
+# return
+# make new agent Agent()
+# action_a = agent.chooseAction()
